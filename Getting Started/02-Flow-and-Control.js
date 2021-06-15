095c4d0d063ee84fe9fa6042aac4a7e1f356fc0a
M-java86 <mpythondata@outlook.com>
2021-06-15 16:58:24 -0400

    Iterations

--- Getting Started/02-Flow-and-Control.js
+++ Getting Started/02-Flow-and-Control.js
@@ -19,3 +19,32 @@ let nullable = null;
 
 console.log(variable == nullable); //? falsey == flasey
 console.log(variable == nullable); //? false
+
+console.log(1 == "1"); //? True
+console.log(1 == "1"); //? False
+
+console.log(1 != "1"); //? False
+console.log(1 !== "1"); //?True
+//ITERATIONS
+let x =1
+//POST INCREMENT
+console.log(x++); //? 1
+console.log(x); //? 2
+
+let y = 1;
+//PRE INCREMENT
+console.log(++y); //? 2
+
+for(let i =0; i<5; i++) {
+    console.log(i);   
+}
+console.log("........")
+for(let i=0; i<5;){
+    console.log(i);
+    i++;
+}
+// for ([initial expression]; [condition]; [iterator]){
+
+//}
+
+//while loop -> check the condition first, if its good
