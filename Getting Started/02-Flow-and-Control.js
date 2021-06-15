0c9cd28312d0e5ac0b0f80a6bfae70c44004fb25
M-java86 <mpythondata@outlook.com>
2021-06-15 16:31:49 -0400

    Falsey

--- Getting Started/02-Flow-and-Control.js
+++ Getting Started/02-Flow-and-Control.js
@@ -8,3 +8,14 @@ let one = 1;
 
 console.log(bool1 ==one); //? 1 == 2
 console.log(bool2 == bool2);
+
+
+
+// strict equality - check the value and the type!
+console.log(bool1 == one); //? boolean == number 
+
+let variable;
+let nullable = null;
+
+console.log(variable == nullable); //? falsey == flasey
+console.log(variable == nullable); //? false
