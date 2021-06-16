2039dc501410f7242cbd02950ee5dd971035a405
M-java86 <mpythondata@outlook.com>
2021-06-15 21:23:52 -0400

    variables excercise

---
+++ Getting Started/02-variables.js
@@ -0,0 +1,5 @@
+//Use template literals in order to dynamically change the sentence
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = ;
---
+++ Getting Started/variables.js
@@ -0,0 +1,13 @@
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+
+
+typeof(a);// will return null
+typeof(b);// will return String
+typeof(c);// will return number
+typeof(d);// will return boolean
+typeof(e);// will return object.
