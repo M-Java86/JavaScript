00c68f72149e5df351d23b0aaf16688dd5e0e4d3
M-java86 <mpythondata@outlook.com>
2021-06-15 21:59:26 -0400

    ASI Excercise

---
+++ Getting Started/ASI.js
@@ -0,0 +1,15 @@
+//ASI- is Automatic Semi-Colon insertion and can be a fiddly aspect of JavaScript
+
+//Declare a few variables / code blocks and terminate the code appropriately.
+
+//1.Remove the semi-colons from the variables / 
+//code-blocks that you've written and see if there is an affect on the output.
+
+//2.Have a look at the below code and try to spot where there might be an error:
+
+function sayHello() {
+    return // A semi-colon after return and this will alter the value of the function
+    {
+      "Someone says hello";
+    }
+  }
