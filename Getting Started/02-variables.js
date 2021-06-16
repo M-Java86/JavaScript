0f8db1fafba76395b4e31773487e945b04b0953e
M-java86 <mpythondata@outlook.com>
2021-06-15 21:26:55 -0400

    solution

--- Getting Started/02-variables.js
+++ Getting Started/02-variables.js
@@ -3,3 +3,14 @@
 let totalMoney = 4000;
 let moneyPaidSoFar = 2348;
 let totalLeftToPay = ;
+
+
+//`The total bill is £4000 the remaining amount of money to be paid is £1652`
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = ;
+
+totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`
