59509035924e2f70a0a31c75110e9ef0a9f772a0
M-java86 <mpythondata@outlook.com>
2021-06-15 22:14:51 -0400

    added solution for FlowChart 2

--- Getting Started/iteration.js
+++ Getting Started/iteration.js
@@ -1,2 +1,33 @@
-//Recreate the following flowchart as a loop >>flowchart located in QA Community under Iterations
+//1.Recreate the following flowchart as a loop >>flowchart located in QA Community under Iterations
+//Iteration and loops allow us to loop over a block of code until a condition is met.
+//If the condition is not satisfied, the code will terminate out of its current execution.
+//Some of the common loops that are used within JavaScript include:
 
+//for Loop
+//While Loop
+//Do While Loop
+//Switch Cases
+
+
+let a = 100;
+while (a <= 200) {
+  a++;
+  console.log(`a = ${a}`);
+}
+
+//soultion printed out in devTools console 
+
+
+//2.Recreate the following flowchart as a loop.
+
+let a = 100;
+while (a <= 200) {
+  if (a % 2 == 0) {
+    console.log("-");
+  } else {
+    console.log("*");
+  }
+  a++;
+}
+
+//Solution printed out in DevTools console
