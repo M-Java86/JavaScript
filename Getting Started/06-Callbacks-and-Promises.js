128aa7a01b5d9034d1eb1214c35ea72186646496
M-java86 <mpythondata@outlook.com>
2021-06-20 22:17:14 -0400

    Updated

--- Getting Started/06-Callbacks-and-Promises.js
+++ Getting Started/06-Callbacks-and-Promises.js
@@ -15,19 +15,49 @@
 //reject -.catch();
 //? the last then -execute no matter what
 
-let myProm = new Promise((reslove, reject)=>{
-    let a = 1; 
-    if(a===1){
-        resolve("Yes this worked");
-     } else{
-         resolve("oh no:(") //or to reject it 
-         //reject("oh no:(")
+'use strict';
 
-    //}
-    //myProm.then((msg))=>{
-       // alert("Hey this worked and we have the following message +msg")
+// promises
 
-    //}
-//Callback
-//A callback is a function that takes in another function as parameter. 
+// Pending - Hasn't been fulfilled or rejected yet
+// Fulfilled/ Success - The action relating to the promise succeeded
+// Rejected/ Failed - The action relating to the promise failed
 
+//resolve - .then();
+//reject - .catch();
+//? The last then - execute no matter what
+
+// let myProm = new Promise((resolve,reject)=>{
+//     let a = 1;
+//     if(a===1){
+//         resolve("Success");
+//     }else{
+//         reject("Failed")
+//     }
+// })
+
+// myProm.then((msg)=>{
+//     alert("Hey this worked and we have the following message "+msg )
+// }).catch((msg)=>{
+//     console.error("Something went wrong " +msg)
+// }).then(()=>{
+//     console.log("No matter what happened before i will still execute!!!!!");
+// })
+
+// console.log(myProm);
+
+// callbacks
+//! a Function that take another Function as a argument
+const sayHello = (fullName)=>{
+    // console.log("HEYYYYYYYYYY "+fullName);
+    alert(`Hello ${fullName}`);
+}
+
+const getInput = ( callBack ) => {
+    console.log(callBack);
+   let getName = prompt("Whats your name!");
+   //invoked just here \/ - look at the ()
+        callBack(getName)
+}
+
+getInput(sayHello);
--- Getting Started/07-Dom-Manipulation.js
+++ Getting Started/07-Dom-Manipulation.js
@@ -7,5 +7,34 @@
 //A Web page is a document. This document can either be displayed in the browser window 
 //or as the HTML source, but it is the same document in both cases. The DOM represents that same document so it can be manipulated / modified with a scripting language, such as JavaScript.
 
-`use strict`
 
+'use strict';
+
+let myDogElment = document.querySelector("#dog");
+
+console.dir(myDogElment);
+myDogElment.alt="this is a fluffy dog!";
+
+myDogElment.setAttribute("width","50%");
+myDogElment.setAttribute("height","50%");
+myDogElment.className="CheckMeOut"
+
+let allParas = document.querySelectorAll("div p");
+console.dir(allParas);
+
+for(let i =0; i<allParas.length;i++){
+    allParas[i].style.backgroundColor ="Red";
+    allParas[i].style.color ="blue";
+}
+
+let myBut = document.createElement("button");
+let mytext = document.createTextNode("Click me!")
+myBut.className="btn btn-danger";
+
+myBut.appendChild(mytext);
+
+console.dir(myBut)
+
+const div = document.querySelector('#addToMe')
+
+    div.appendChild(myBut);
